148573822bc942042bf99defefce2bdfc8bc5ae0
hdaoudi <hdaoudi@student.1337.ma>
2024-10-24 16:16:52 +0100

    isalnum

--- CommonCore/libft/ft_isalnum.c
+++ CommonCore/libft/ft_isalnum.c
@@ -6,19 +6,17 @@
 /*   By: hdaoudi <hdaoudi@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 13:21:38 by hdaoudi           #+#    #+#             */
-/*   Updated: 2024/10/24 16:07:05 by hdaoudi          ###   ########.fr       */
+/*   Updated: 2024/10/24 16:15:45 by hdaoudi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_isalnum (int c)
+int	ft_isalnum(int c)
 {
-    unsigned char	n;
+	unsigned char	n;
 
-    n = (unsigned char ) c;
-    
-    return (((n >= 'a' && n <= 'z') || 
-        (n >= 'A' && n <= 'Z')) || (n >= '0' && n <= '9'));
-    //return ( ft_isalpha (n) || ft_isdigit (n));
+	n = (unsigned char ) c;
+	return (((n >= 'a' && n <= 'z')
+			|| (n >= 'A' && n <= 'Z')) || (n >= '0' && n <= '9'));
 }
