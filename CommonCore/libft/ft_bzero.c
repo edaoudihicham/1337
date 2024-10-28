4c101556c81b1ea4db2e5b218b4bfa9537552539
edaoudihicham <edaoudihicham@gmail.com>
2024-10-28 10:22:20 +0100

    ss

--- CommonCore/libft/ft_bzero.c
+++ CommonCore/libft/ft_bzero.c
@@ -6,7 +6,7 @@
 /*   By: hdaoudi <hdaoudi@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 13:37:05 by hdaoudi           #+#    #+#             */
-/*   Updated: 2024/10/26 15:42:00 by hdaoudi          ###   ########.fr       */
+/*   Updated: 2024/10/27 15:51:27 by hdaoudi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,11 @@ void	ft_bzero(void *s, size_t n)
 	unsigned char	*temp;
 
 	temp = (unsigned char *)s;
-	/* while (n)
-	{
-		*temp++ = 0;
-		n--;
-	} */ 
+	// while (n)
+	// {
+	// 	*temp++ = 0;
+	// 	n--;
+	// }  
 
 	ft_memset(temp, 0, n);
 }
