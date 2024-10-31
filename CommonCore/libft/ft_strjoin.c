9c62da80a0ebe443617dafc30506f46e108cab56
hdaoudi <hdaoudi@student.1337.ma>
2024-10-31 21:45:39 +0100

    ss

--- CommonCore/libft/ft_strjoin.c
+++ CommonCore/libft/ft_strjoin.c
@@ -6,7 +6,24 @@
 /*   By: hdaoudi <hdaoudi@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 21:29:23 by hdaoudi           #+#    #+#             */
-/*   Updated: 2024/10/31 21:29:24 by hdaoudi          ###   ########.fr       */
+/*   Updated: 2024/10/31 21:45:08 by hdaoudi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	int		len1;
+	int		len2;
+	char	*str;
+
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	str = (char *)malloc((len1 + len2 + 1) * (sizeof(char)));
+	if (str == NULL)
+		return (NULL);
+	ft_strlcpy(str, s1, len1);
+	ft_strlcat(str, s2, len2);
+	return (str);
+}
--- CommonCore/libft/libft.h
+++ CommonCore/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: hdaoudi <hdaoudi@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 16:41:21 by hdaoudi           #+#    #+#             */
-/*   Updated: 2024/10/31 18:00:10 by hdaoudi          ###   ########.fr       */
+/*   Updated: 2024/10/31 21:29:52 by hdaoudi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,5 +43,5 @@ int		ft_atoi(const char *str);
 void	*ft_calloc(size_t count, size_t size);
 char	*ft_strdup(const char *s1);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
-
+char	*ft_strjoin(char const *s1, char const *s2);
 #endif
