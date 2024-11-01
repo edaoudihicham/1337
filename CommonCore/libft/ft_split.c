ed50764565d3b3c2011ccaed4d5ed82bde71356c
hdaoudi <hdaoudi@student.1337.ma>
2024-11-01 20:29:41 +0100

    splitsss

--- CommonCore/libft/ft_split.c
+++ CommonCore/libft/ft_split.c
@@ -6,20 +6,33 @@
 /*   By: hdaoudi <hdaoudi@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/01 12:57:19 by hdaoudi           #+#    #+#             */
-/*   Updated: 2024/11/01 13:10:39 by hdaoudi          ###   ########.fr       */
+/*   Updated: 2024/11/01 20:17:16 by hdaoudi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-
-char	**ft_split(char const *s, char c)
+// #include "libft.h"
+ #include <stdio.h>
+int countwords(char const *s, char c)
 {
 	int i;
+	int j;
 
-	i = 0;	
-	while (s[i] == 'c' )
-		i++;
-	
+	i = 0;
+	j = 0;
+	while(s[i])
+	{
+		if(s[i] == c)
+			j++;
+	i++;
+	}
+	return (j);
+}
+// char	**ft_split(char const *s, char c)
+// {
+// }
 
-		
+int main ()
+{
+	printf("%d", countwords("hey hey hey", ' '));
+	return 0;
 }
