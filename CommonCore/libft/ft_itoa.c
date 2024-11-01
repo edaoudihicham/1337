eadcbf56f228fe6546bcd3847d71c089d03c49b6
hdaoudi <hdaoudi@student.1337.ma>
2024-11-01 18:16:46 +0100

    ss

--- CommonCore/libft/ft_itoa.c
+++ CommonCore/libft/ft_itoa.c
@@ -6,33 +6,36 @@
 /*   By: hdaoudi <hdaoudi@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/01 12:44:02 by hdaoudi           #+#    #+#             */
-/*   Updated: 2024/11/01 17:42:55 by hdaoudi          ###   ########.fr       */
+/*   Updated: 2024/11/01 18:16:34 by hdaoudi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
 // #include <limits.h>
 
 static int	slen(int n)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	while (n > 0)
 	{
 		n /= 10;
-		i++;	
+		i++;
 	}
-return (i);
+	return (i);
 }
-static char *filler(int i, int n, int isnegative)
+
+static char	*filler(int i, int n, int isnegative)
 {
-	char *str;
+	char	*str;
+
 	str = (char *)malloc((i + 1) * sizeof(char));
 	if (str == NULL)
 		return (NULL);
 	str[i--] = '\0';
-	while(n > 0)
+	while (n > 0)
 	{
 		str[i--] = (n % 10) + '0';
 		n /= 10;
@@ -41,11 +44,12 @@ static char *filler(int i, int n, int isnegative)
 		str[0] = '-';
 	return (str);
 }
+
 char	*ft_itoa(int n)
 {
 	int	isnegative;
 	int	i;
-	
+
 	isnegative = 0;
 	i = slen(n);
 	if (n == -2147483648)
@@ -58,12 +62,11 @@ char	*ft_itoa(int n)
 		isnegative = 1;
 		i = 1 + slen(n);
 	}
-	
-	return (filler(i, n ,isnegative));
+	return (filler(i, n, isnegative));
 }
 // #include <stdio.h>
 // int main()
 // {
 // 	printf("%s", ft_itoa(-00000001));
-// 	return 0;
+// 	return (0);
 // }
