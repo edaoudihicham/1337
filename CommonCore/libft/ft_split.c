fff8ed0d34013b66b0dd84e29d7bedab94811c44
edaoudihicham <edaoudihicham@gmail.com>
2024-11-02 15:23:16 +0100

    eee

--- CommonCore/libft/ft_split.c
+++ CommonCore/libft/ft_split.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hdaoudi <hdaoudi@student.1337.ma>          +#+  +:+       +#+        */
+/*   By: hdaoudi <hdaoudi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/01 12:57:19 by hdaoudi           #+#    #+#             */
-/*   Updated: 2024/11/02 14:02:37 by hdaoudi          ###   ########.fr       */
+/*   Updated: 2024/11/02 15:19:04 by hdaoudi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,9 +30,19 @@ int countwords(char const *s, char c)
 // char	**ft_split(char const *s, char c)
 // {
 // 	char **ptr;
-// 	**ptr = (char **)malloc(sizeof(*s) * countwords(*s, c));
-// 	if (**ptr == NULL)
+
+// 	ptr = (char **)malloc(sizeof(*s) * countwords(*s, c)); 
+// 	if (ptr == NULL)
 // 		return (NULL);
+// loop all the string s
+//{ while (*s != '\0')
+// skip the set  if s is \0 and s != c
+//count = 0;
+// clacul the len of the string : count while (s[count] && s[count] != c) count++;
+// p[i++] = substr(s, 0, count);
+// s = s + count;
+//}
+// p[i] = 0;
 // }
 
 int main ()
