bee783bd930b101c5cae5241fca77336d1b9db04
edaoudihicham <edaoudihicham@gmail.com>
2024-11-05 15:37:26 +0100

    lists.c

--- CommonCore/Lists.c
+++ CommonCore/Lists.c
@@ -1,55 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct node{
-	int number;
-	struct node* next;
-}node;
-
-int main()
+typedef struct s_list
 {
-	node *list = NULL;
+	int				n;
+	struct s_list	*next;
+}					t_list;
 
-	node *n = malloc(sizeof(node));
-	if (n == NULL)
-		return 1;
-	n->number = 1; // n points to the node
-	n->next = NULL;
-	list = n; // Update list to point to n, making list point to the first node in the linked list. = we no longer need n
+int	main(void)
+{
+	t_list	*root;
+	t_list	*temp;
+	t_list *current;
+
+	root = malloc(sizeof(t_list));
+	if (root == NULL) return (1);
+	root->n = 1;
+	root->next = malloc(sizeof(t_list));
+	if (root->next == NULL) return 1;
+	root->next->n = 2;
+	root->next->next = malloc(sizeof(t_list));
+	if (root->next->next == NULL) return 1;
+	root->next->next->n = 3;
+
+	root->next->next->next = NULL;
 	
-	n = malloc(sizeof(node));
-	if (n == NULL)
-{
-	free(list);
-	return 1;
-}
-	n->number = 2;
-	n->next = NULL;
-	list->next = n; //now my first node points to the second node we no longer need n
-
-	n = malloc(sizeof(node));
-	if (n == NULL)
-{
-	free(list->next); // we free the last node first
-	free(list);// if we free this one first we will lose trace of the next
-	return 1;
-}
-	n-> number = 3;
-	n->next = NULL;
-	list->next->next = n; // now our second next points at n. We will do this with while loop later
-
-	node *tmp = list;
-	while(tmp != NULL)
+	// iteration
+	current = root;
+	while (current != NULL)
 	{
-		printf("%d\n", tmp->number);
-		tmp = tmp->next;
+		printf("%d\n", current->n);
+		current = current->next; // i++
 	}
-	while(list != NULL)
+	// Free allocated memory
+	current = root;
+	while (current != NULL)
 	{
-			node* tmp = list->next;
-			free(list);
-			list = tmp;
+		temp = current->next;
+		free(current);
+		current = temp;
 	}
-
-
+	return (0);
 }
