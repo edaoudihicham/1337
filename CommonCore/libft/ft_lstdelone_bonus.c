daa840ba57ba785dbd3a5c12aa8234022d244c30
edaoudihicham <edaoudihicham@gmail.com>
2024-11-05 23:44:54 +0100

    e

--- CommonCore/libft/ft_lstdelone_bonus.c
+++ CommonCore/libft/ft_lstdelone_bonus.c
@@ -6,7 +6,7 @@
 /*   By: hdaoudi <hdaoudi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 23:21:28 by hdaoudi           #+#    #+#             */
-/*   Updated: 2024/11/05 23:43:40 by hdaoudi          ###   ########.fr       */
+/*   Updated: 2024/11/05 23:44:46 by hdaoudi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	ft_lstdelone(t_list *lst, void (*del)(void *))
 {
-	if (lst != NULL || del != NULL)
+	if (lst == NULL || del == NULL)
 		return ;
 	del(lst->content);
 	free(lst);
