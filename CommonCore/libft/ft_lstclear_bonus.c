2e3dcf916d69f629a864adb13981135422ba8ab4
edaoudihicham <edaoudihicham@gmail.com>
2024-11-05 23:55:58 +0100

    clear

--- CommonCore/libft/ft_lstclear_bonus.c
+++ CommonCore/libft/ft_lstclear_bonus.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*   ft_lstclear_bonus.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hdaoudi <hdaoudi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/05 11:21:08 by hdaoudi           #+#    #+#             */
-/*   Updated: 2024/11/05 11:21:51 by hdaoudi          ###   ########.fr       */
+/*   Created: 2024/11/05 23:50:08 by hdaoudi           #+#    #+#             */
+/*   Updated: 2024/11/05 23:55:45 by hdaoudi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,18 @@
 
 void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
-	
+	t_list	*current;
+	t_list	*tmp;
+
+	if (lst == NULL || del == NULL)
+		return ;
+	current = *lst;
+	while (current != NULL)
+	{
+		tmp = current;
+		current = current->next;
+		del(tmp->content);
+		free(tmp);
+	}
+	*lst = NULL;
 }
