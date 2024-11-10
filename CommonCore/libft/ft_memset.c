b72eefb32188b6f494ddf07b61022475978a202c
edaoudihicham <edaoudihicham@gmail.com>
2024-11-10 15:02:59 +0100

    ss

--- CommonCore/libft/ft_memset.c
+++ CommonCore/libft/ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: hdaoudi <hdaoudi@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 13:32:35 by hdaoudi           #+#    #+#             */
-/*   Updated: 2024/11/10 11:17:53 by hdaoudi          ###   ########.fr       */
+/*   Updated: 2024/11/10 15:02:45 by hdaoudi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,6 @@ void	*ft_memset(void *b, int c, size_t len)
 // int main ()
 // {
 // 	char arr[5];
-	
 // 	ft_memset(&arr[0],'-',1);
 // 	ft_memset(&arr[1],'1',1);
 // 	ft_memset(&arr[2],'3',1);
